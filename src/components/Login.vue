58c152b8e1e60ae4e5fe378a040d8fb9e4c6d99b
h_admin <709820314@qq.com>
2021-01-28 23:18:00 +0800

    增添登录弹窗提示

---
+++ src/components/Login.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="login_container">
+      <div class="login_box">
+        <!--头像区域-->
+        <div class="avatar_box">
+          <img src="../assets/logo.png" alt="">
+        </div>
+        <!--登录表单区域-->
+        <el-form ref="loginFormRef" label-width="0px" class="login_form" :model="login_form" :rules="loginFormRules">
+          <el-form-item prop="username">
+            <el-input prefix-icon="iconfont iconuser" v-model="login_form.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="iconfont iconmima1" v-model="login_form.password" type="password"></el-input>
+          </el-form-item>
+          <el-form-item class="btns">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      login_form: {
+        username: '',
+        password: ''
+      },
+      loginFormRules: {
+        username: [
+          {required: true, message: '请输入登录名称', trigger: 'blur'},
+          {min: 3, max: 10, message: '长度在3到10个字符', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 6, max: 15, message: '长度在6到15位', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    resetLoginForm () {
+      console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        console.log(valid)
+        if (!valid) return
+        const {data: res} = await this.$http.post('login', this.login_form)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败')
+        } else {
+          return this.$message.success('登录成功')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+  .login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+  .avatar_box{
+    height: 130px;
+    width: 130px;
+    border: solid 1px #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #eee;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: #fff;
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+  .btns{
+    display: flex;
+    justify-content: flex-end;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/css/global.css'
+import ElementUI, {Message} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
+Vue.prototype.$message = Message
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
